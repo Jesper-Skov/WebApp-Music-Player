cd59399cc2a60008d19d993fec8fcd8d982bb5b6
Jesper-Skov <31208883+Jesper-Skov@users.noreply.github.com>
2017-10-31 17:36:11 +0100

    Made the delete button work
    Made the minus button hide the elements it is relevant to.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
 // JavaScript Document
 //Found this code here on w3school https://www.w3schools.com/jquery/html_clone.asp
 $(document).ready(function () {
-	$("#addone").click(function () {
-		$("#mone").clone().appendTo("#waitlist");
-	});
-	$("#addtwo").click(function () {
-		$("#mtwo").clone().appendTo("#waitlist");
-	});
-	$("#addthree").click(function () {
-		$("#mthree").clone().appendTo("#waitlist");
-	});
-	$("#addfour").click(function () {
-		$("#mfour").clone().appendTo("#waitlist");
-	});
-	$("#addfive").click(function () {
-		$("#mfive").clone().appendTo("#waitlist");
-	});
-	$("#listaddone").click(function () {
-		$("#mone").clone().appendTo("#waitlist");
-	});
-	$("#listaddtwo").click(function () {
-		$("#mtwo").clone().appendTo("#waitlist");
-	});
-	$("#listaddthree").click(function () {
-		$("#mthree").clone().appendTo("#waitlist");
-	});
-	$("#listaddfour").click(function () {
-		$("#mfour").clone().appendTo("#waitlist");
-	});
-	$("#listaddfive").click(function () {
-		$("#mfive").clone().appendTo("#waitlist");
-	});
+    $("#addone").click(function () {
+        $("#mone").clone().appendTo("#waitlist");
+    });
+    $("#addtwo").click(function () {
+        $("#mtwo").clone().appendTo("#waitlist");
+    });
+    $("#addthree").click(function () {
+        $("#mthree").clone().appendTo("#waitlist");
+    });
+    $("#addfour").click(function () {
+        $("#mfour").clone().appendTo("#waitlist");
+    });
+    $("#addfive").click(function () {
+        $("#mfive").clone().appendTo("#waitlist");
+    });
+    $("#listaddone").click(function () {
+        $("#mone").clone().appendTo("#waitlist");
+    });
+    $("#listaddtwo").click(function () {
+        $("#mtwo").clone().appendTo("#waitlist");
+    });
+    $("#listaddthree").click(function () {
+        $("#mthree").clone().appendTo("#waitlist");
+    });
+    $("#listaddfour").click(function () {
+        $("#mfour").clone().appendTo("#waitlist");
+    });
+    $("#listaddfive").click(function () {
+        $("#mfive").clone().appendTo("#waitlist");
+    });
+   //Found the hide effect here: https://www.w3schools.com/jquery/jquery_hide_show.asp
+    $("#denyone").click(function () {
+        $("#hideone").hide();
+    });
+    $("#denytwo").click(function () {
+        $("#hidetwo").hide();
+    });
+    $("#denythree").click(function () {
+        $("#hidethree").hide();
+    });
+    $("#denyfour").click(function () {
+        $("#hidefour").hide();
+    });
+    $("#denyfive").click(function () {
+        $("#hidefive").hide();
+    });
 });
 //This is the javascript that makes the list work https://www.w3schools.com/howto/howto_js_filter_lists.asp
 function myFunction() {
